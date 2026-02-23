38cccf35e3363c39fa80d030d8f4d407c7e16653
Morten Jensen <mo_ten@hotmail.com>
2026-02-23 12:34:51 +0100

    Added test and logback for project

---
+++ src/test/java/app/persistence/MovieDAOTest.java
@@ -0,0 +1,17 @@
+package app.persistence;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
