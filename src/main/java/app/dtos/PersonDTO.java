dc96dbfaed1ffb8d90c9cd335c913feacca181be
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-02-23 23:46:06 +0100

    Added PersonDTO. Updated Gender and Role - added methods to return ENUM OR value

---
+++ src/main/java/app/dtos/PersonDTO.java
@@ -0,0 +1,41 @@
+package app.dtos;
+
+import app.enums.Gender;
+import app.enums.Role;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record PersonDTO(
+        @JsonProperty("id")
+        int movieId,
+
+        @JsonProperty("gender")
+        int gender,
+
+        @JsonProperty("known_for_department")
+        String role,
+
+        @JsonProperty("name")
+        String name,
+
+        @JsonProperty("original_name")
+        String originalName,
+
+        @JsonProperty("cast_id")
+        int castId,
+
+        @JsonProperty("character")
+        String character
+)
+{
+    public Gender getGenderEnum() // TODO Rename
+    {
+        return Gender.fromValue(gender);
+    }
+
+    public Role getRoleEnum() // TODO Rename
+    {
+        return Role.fromValue(role);
+    }
+}
--- src/main/java/app/enums/Gender.java
+++ src/main/java/app/enums/Gender.java
@@ -2,6 +2,30 @@ package app.enums;
 
 public enum Gender
 {
-    FEMALE,
-    MALE
+    FEMALE(1),
+    MALE(2);
+
+    private final int value;
+
+    Gender(int value)
+    {
+        this.value = value;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+
+    public static Gender fromValue(int value)
+    {
+        for (Gender g : values())
+        {
+            if (g.value == value)
+            {
+                return g;
+            }
+        }
+        throw new IllegalArgumentException("Unknown Gender value: " + value);
+    }
 }
