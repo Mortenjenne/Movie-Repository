172d901a53aad54c1933282b3ee1b6b41ba9914e
Morten Jensen <mo_ten@hotmail.com>
2026-02-23 16:15:05 +0100

    Implemented create, get and getall

--- src/main/java/app/persistence/MovieDAO.java
+++ src/main/java/app/persistence/MovieDAO.java
@@ -2,33 +2,98 @@ package app.persistence;
 
 import app.entities.Movie;
 import app.persistence.daos.IMovieDAO;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.TypedQuery;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class MovieDAO implements IMovieDAO
 {
+    private final EntityManagerFactory emf;
+
+    public MovieDAO(EntityManagerFactory emf)
+    {
+        this.emf = emf;
+    }
+
     @Override
-    public Movie create(Movie movie) {
+    public Movie create(Movie movie)
+    {
+        validateNotNull(movie);
+
+        try(EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+            em.persist(movie);
+            em.getTransaction().commit();
+
+            return movie;
+        }
+    }
+
+    @Override
+    public Set<Movie> getAll()
+    {
+        try(EntityManager em = emf.createEntityManager())
+        {
+            TypedQuery<Movie> query = em.createQuery("SELECT m FROM Movie m", Movie.class);
+
+            return new HashSet<>(query.getResultList());
+        }
+    }
+
+    @Override
+    public Movie update(Movie movie)
+    {
         return null;
     }
 
     @Override
-    public Set<Movie> getAll() {
-        return Set.of();
-    }
-
-    @Override
-    public Movie update(Movie movie) {
-        return null;
-    }
-
-    @Override
-    public boolean delete(Long id) {
+    public boolean delete(Long id)
+    {
         return false;
     }
 
     @Override
-    public Movie getByID(Long id) {
-        return null;
+    public Movie getByID(Long id)
+    {
+        validateId(id);
+
+        try(EntityManager em = emf.createEntityManager())
+        {
+            TypedQuery<Movie> query = em.createQuery("SELECT m FROM Movie m WHERE m.id = :id", Movie.class)
+                    .setParameter("id", id);
+            Movie movie = query.getSingleResult();
+            checkMovieExists(id, movie);
+
+            return movie;
+        }
+    }
+
+    private void checkMovieExists(Long id, Movie movie)
+    {
+        if (movie == null)
+        {
+            throw new EntityNotFoundException("Movie with ID " + id + " was not found.");
+        }
+    }
+    
+    private void validateNotNull(Movie movie)
+    {
+        if (movie == null)
+        {
+            throw new IllegalArgumentException("Movie" + " cannot be null.");
+        }
+    }
+
+    private void validateId(Long id)
+    {
+        if (id == null || id <= 0)
+        {
+            throw new IllegalArgumentException("Invalid ID: Must be provided and greater than 0.");
+        }
     }
 }
