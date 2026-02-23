f28957ad37ad7d251f587ff53b0e7ef819764eb8
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-02-23 23:44:57 +0100

    Added GenreDTO

---
+++ src/main/java/app/dtos/GenreDTO.java
@@ -0,0 +1,15 @@
+package app.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record GenreDTO(
+        @JsonProperty("id")
+        int movieId,
+
+        @JsonProperty("name")
+        String name
+)
+{
+}
