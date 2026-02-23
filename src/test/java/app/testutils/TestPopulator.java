adc39f22631eb566dd2dfdf33ffd4342dd681b1c
Morten Jensen <mo_ten@hotmail.com>
2026-02-23 17:14:25 +0100

    Addded bidirectional to Movie to Cast so Movie can Cascade.all on Cast

--- src/test/java/app/testutils/TestPopulator.java
+++ src/test/java/app/testutils/TestPopulator.java
@@ -1,4 +1,32 @@
 package app.testutils;
 
-public class TestPopulator {
+import app.entities.IEntity;
+import app.persistence.daos.IMovieDAO;
+
+import java.util.Map;
+
+public class TestPopulator
+{
+    private final IMovieDAO movieDAO;
+    private final Map<String, IEntity> seeded;
+
+    public TestPopulator(IMovieDAO movieDAO, Map<String, IEntity> seeded)
+    {
+        this.movieDAO = movieDAO;
+        this.seeded = seeded;
+    }
+
+    public Map<String, IEntity> getSeededData()
+    {
+        return seeded;
+    }
+
+    public void populate()
+    {
+        populateMovies();
+    }
+
+    private void populateMovies()
+    {
+    }
 }
