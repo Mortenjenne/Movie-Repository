dd485cb39f39e738767253b42f39deb8aa5f75c1
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-02-25 12:48:22 +0100

    Fixed typo

--- src/main/java/app/dtos/tmdb/TMDBGenreDTO.java
+++ src/main/java/app/dtos/tmdb/TMDBGenreDTO.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record GenreDTO(
+public record TMDBGenreDTO(
         @JsonProperty("id")
         Long movieId,
 
